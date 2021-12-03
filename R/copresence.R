2a39604e30f449fc722ae301be7035f5eb710207
Nick Gestrich <gestrich@uni-frankfurt.de>
2021-12-03 13:56:58 +0100

    cleaning copresence function

--- R/copresence.R
+++ R/copresence.R
@@ -1,5 +1,4 @@
 # co-presence function (after Peeples 2017, http://mattpeeples.net/netstats.html). This creates a square matrix with 1 and 0 for presence and absence.
-
 co.p <- function(x, thresh = 0.1) {
   # create matrix of proportions
   temp <- prop.table(as.matrix(x), 1)
